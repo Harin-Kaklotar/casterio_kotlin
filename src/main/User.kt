eed403c96753258febd167ead7fac02408b883f3
HarinKaklotar <harinlive@gmail.com>
2020-06-21 13:30:32 +0530

    class setter property

--- src/main/User.kt
+++ src/main/User.kt
@@ -4,4 +4,11 @@ class User (var firstName: String, var lastName: String){
 
     var fullName = "$firstName $lastName"
         get() = "Full name : $field"
+        set(value) {
+            if (value.startsWith("Android")){
+                field = "Android Developer"
+            }else{
+                field = value
+            }
+        }
 }
--- src/main/t10.kt
+++ src/main/t10.kt
@@ -3,4 +3,10 @@ package main
 fun main(){
     var harin = User("Harin", "Kaklotar")
     println(harin.fullName)
+
+    harin.fullName = "Android"
+    println(harin.fullName)
+
+    harin.fullName = "Hello world"
+    println(harin.fullName)
 }
